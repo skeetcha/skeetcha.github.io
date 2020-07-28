a162f402b2fea4a5827ada342ac3ef22b0be2d94
Daniel Unterholzner <pianojammer@comcast.net>
2020-07-28 14:10:58 -0700

    Updates

---
+++ precache-manifest.8dbc518d2d2236c56259f8a10493f83e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18c0c8ddc77a95a2d3b4c42cecbf8b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "583876d34fcbf6fb4256",
+    "url": "/static/css/main.ecbf7978.chunk.css"
+  },
+  {
+    "revision": "a4b6418d08f2a35fadd3",
+    "url": "/static/js/2.781dde76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.781dde76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "583876d34fcbf6fb4256",
+    "url": "/static/js/main.26eeacf1.chunk.js"
+  },
+  {
+    "revision": "925004bce61d9c2dd9b0",
+    "url": "/static/js/runtime-main.eb86ec6a.js"
+  },
+  {
+    "revision": "af2afaf616e88cc70edec61645cd7b45",
+    "url": "/static/media/self.af2afaf6.png"
+  }
+]);
