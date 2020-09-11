f06f0127a77f7a0d1bf78ed34c7572ebf3afb584
Daniel Unterholzner <pianojammer@comcast.net>
2020-09-11 11:45:16 -0700

    Adding in scripts

--- scripts/project-main.js
+++ scripts/project-main.js
@@ -1,6 +1,30 @@
 var update_texts = function() { $('body').i18n(); };
 var get_lang_code = function(el) { return el.attributes["data-locale"].value; };
 
+function update_portfolio_buttons(projectName) {
+    if (projectName === 'mememachine') {
+        $('.button:first-of-type a').click(function(e) {
+            e.preventDefault();
+        });
+
+        $('.button:last-of-type a').click(function(e) {
+            e.preventDefault();
+            window.location = 'portfolio-item.html#shutin';
+            window.location.reload();
+        });
+    } else if (projectName === 'shutin') {
+        $('.button:first-of-type a').click(function(e) {
+            e.preventDefault();
+            window.location = 'portfolio-item.html#mememachine';
+            window.location.reload();
+        });
+
+        $('.button:last-of-type a').click(function(e) {
+            e.preventDefault();
+        });
+    }
+}
+
 jQuery(document).ready(function($){
 	var isLateralNavAnimating = false;
 	
@@ -18,6 +42,15 @@ jQuery(document).ready(function($){
 			});
 		}
     });
+
+    var projectName = window.location.hash.substr(1);
+    
+    if (!($('#' + projectName).hasClass('active-project'))) {
+        $('.active-project').removeClass('active-project');
+        $('#' + projectName).addClass('active-project');
+    }
+
+    update_portfolio_buttons(projectName);
     
     $.i18n().load({
         "en": {
@@ -35,6 +68,7 @@ jQuery(document).ready(function($){
             "project.mememachine.description": "Download, Rate, and Create Memes - Programmer",
             "project.mememachine.long-description.1": "This was a project created by the University of the Pacific chapter of the Association for Computing Machinery in March of 2017 to create a set of software to download and aggregate memes from different social media sites to which memes are uploaded, rate them by some sort of combined metric made up of data from each of the different sites, and then use all of that data to then create its own memes. The project was discontinued roughly a month later.",
             "project.mememachine.long-description.2": "The project uses primarily Python in its code, although one of us wanted to use Haskell to download memes from ImgFlip's meme generator. I looked a little at Haskell although I wouldn't say that I can explain line by line what the code does.",
+            "project.mememachine.link": "GitHub Repository",
             "project.shutin.title": "Shut In",
             "project.shutin.description": "Short Film - Gaffer, Foley Artist, Sound Editor",
             "project.shutin.long-description.1": "This was a short film created as part of the Go Film! Workshop held at the San Joaquin County Office of Education in June 2012. During the first of two weeks, we learned the fundamentals of creating a short film and then we pitched our projects and chose what would end up being the final product. The second week then comprised of pre-production, filming over the course of a full day on-location at a daycare in Manteca, CA, and then finishing up post-production until late into the night.",
@@ -60,6 +94,7 @@ jQuery(document).ready(function($){
             "project.mememachine.description": "Memes herunterladen, bewerten, und schaffen - Programmierer",
             "project.mememachine.long-description.1": "",
             "project.mememachine.long-description.2": "",
+            "project.mememachine.link": "",
             "project.shutin.title": "Shut In",
             "project.shutin.description": "Kurzfilm - Gaffer, Geräuschemacher, Tonmeister",
             "project.shutin.long-description.1": "",
