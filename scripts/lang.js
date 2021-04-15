25db73a0a24b1bec268011d389d9b3ff956e7253
Daniel Unterholzner <pianojammer@comcast.net>
2021-04-15 16:18:02 -0700

    Adding in German text; Adding in The Four Woods Podcast texts

--- scripts/lang.js
+++ scripts/lang.js
@@ -47,6 +47,12 @@ var language_data = {"en": {
     "project.cansat.long-description": "During my junior year of high school, my dad approached me with an offer to work on a project involving a simulated sattelite in an enclosure the size and shape of a soda can run by an Arduino. He connected me and two of his students to Ted Tagami of Magnitude.io and we were all connected with Dr. Elizabeth Basha and her students at University of the Pacific. The CanSat project was eventually launched into low-Earth orbit in November 2019.",
     "project.cansat.link": "More Information",
 
+    "project.fourwoods.skills": "",
+    "project.fourwoods.title": "The Four Woods Podcast",
+    "project.fourwoods.description": "An audio drama podcast about myth and magic - Writer, Director, Editor, Producer",
+    "proejct.fourwoods.long-description": "",
+    "project.fourwoods.link": "",
+
     "projects.title": "Projects",
     "projects.skills.label": "Skills:",
 
@@ -85,6 +91,101 @@ var language_data = {"en": {
     "contact.title": "Contact Me",
     "contact.placeholder.name": "Name *",
     "contact.placeholder.email": "Email *",
+    "contact.placeholder.message": "Message *",
     "contact.submit": "Submit"
 },
-"de":{}};
+"de": {
+    "socials.instagram": "Instagram",
+    "socials.twitter": "Twitter",
+    "socials.facebook": "Facebook",
+    "socials.youtube": "YouTube",
+    "socials.tiktok": "TikTok",
+    "socials.twitch": "Twitch",
+
+    "menu.showcase": "Projekte",
+    "menu.blog": "Blog",
+    "menu.about": "Biographie",
+    "menu.contact": "Kontaktieren",
+
+    "title": "CityboundForest",
+    "title.message": "Hallo ich bin **Cass Unterholzner.**  \nIch bin Geschichtenerzähler, wer mit !1[**Film**], !2[**Theatre**], !3[**Videospiele**], und !4[**Musik**] arbeiten bin.",
+
+    "footer.copyright": "© Copyright Cass Unterholzner 2020.",
+
+    "project.mememachine.skills": "Python, Python Reddit Aggregator Wrapper (PRAW), ImgurPython",
+    "project.mememachine.title": "Meme Machine",
+    "project.mememachine.description": "Meme herunterladen, bewerten, und machen - Programmierer",
+    "project.mememachine.long-description": "The Association for Computing Machinery, University of the Pacific chapter created a project in March 2017 that downloads and aggregates memes from various social media sites including Reddit, Imgur, Tumblr, and Imgflip using Python and Haskell. It included a plan to write a module that would gather ratings and use the data as input for machine learning to create its own memes.",
+    "project.mememachine.link": "GitHub Repository",
+
+    "project.shutin.skills": "Final Cut Pro X, Adobe Premiere Pro, Foley-Kunst, Beleuchtung Aufbau",
+    "project.shutin.title": "Shut In",
+    "project.shutin.description": "Kurzfilm - Gaffer, Foley-Künstler, Tonmeister",
+    "project.shutin.long-description": "The San Joaquin County Office of Education held a two-week filmmaking workshop in June 2012. After a full day of shooting at a daycare in Manteca, CA, the crew premiered the film at the Lincoln High School theatre to a packed house. The crew then held a Q and A session with those in attendance.",
+    "project.shutin.link": "IMDB Link",
+
+    "project.bulletrush.skills": "Unity, C#, Projektmanagement, agile Entwicklung",
+    "project.bulletrush.title": "Bullet Rush",
+    "project.bulletrush.description": "Cuphead kombiniert mit FPS Spiele - Programmierer, Projektleiter",
+    "project.bulletrush.long-description": "I took the Computer Game Technologies course at University of the Pacific in Fall 2019 to learn Video Game Design and Development. After pitching ideas to the class similarly to my earlier Application Development course, I was placed as the Project Manager of the Bullet Rush team along with a Lead Programmer, a general Programmer, and a Media Director. At the end of the course, we presented to the community for feedback.",
+    "project.bulletrush.link": "Bitbucket Respository",
+
+    "project.dndcombatsim.skills": "Java, JavaScript, Projektmanagement",
+    "project.dndcombatsim.title": "Dungeons and Dragons Combat Simulator",
+    "project.dndcombatsim.description": "Bekämpfung-Simulator für die fünfte Ausgabe des RPG Dungeons and Dragons - Programmierer, Projektleiter",
+    "project.dndcombatsim.long-description": "I took the Application Development course at University of the Pacific to learn how to work on a team to develop an application. In our groups, we proposed our final projects for the class and began our work. There were no roles, so I took initiative to be the Project Manager along with doing basic coding on the JSON data and general backend programming.",
+    "project.dndcombatsim.link": "Because we used data from 5e.tools, I am not including a link to the repository here.",
+
+    "project.cansat.skills": "C++, Arduino",
+    "project.cansat.title": "CanSat",
+    "project.cansat.description": "Ein simulierter Satellit in einem Gehäuse mit den Form und Größe des Soda-Doses - Programmierer",
+    "project.cansat.long-description": "During my junior year of high school, my dad approached me with an offer to work on a project involving a simulated sattelite in an enclosure the size and shape of a soda can run by an Arduino. He connected me and two of his students to Ted Tagami of Magnitude.io and we were all connected with Dr. Elizabeth Basha and her students at University of the Pacific. The CanSat project was eventually launched into low-Earth orbit in November 2019.",
+    "project.cansat.link": "Weitere Informationen",
+
+    "project.fourwoods.skills": "",
+    "project.fourwoods.title": "The Four Woods Podcast",
+    "project.fourwoods.description": "An audio drama podcast about myth and magic - Writer, Director, Editor, Producer",
+    "proejct.fourwoods.long-description": "",
+    "project.fourwoods.link": "",
+
+    "projects.title": "Projekte",
+    "projects.skills.label": "Können:",
+
+    "about.quote": "\"Creativity is an energy. It's a precious energy, and it's something to be protected. A lot of people take for granted that they're a creative person, but I know from experience, feeling it in myself, it is a magic; it is an energy. And it can't be taken for granted.\" --Ava DuVernay",
+
+    "about.bio": "Every person is a story, and storyteller Cass Unterholzner is no different. They have been telling stories since they were younger. Their creative cravings have been sought after through !1[**short films**], !2[**acting**] and playing music in !2[**musical theatre**], !3[**developing**] and critiquing !3[**video games**], and !4[**writing**] and !4[**recording music**].\n\n\n\nCass is currently producing an !2[**audio drama podcast**] titled The Four Woods Podcast after developing it as a serial novel story with inspirations from the Harry Potter and Percy Jackson novels.\n\n\n\nEven before entering high school, Cass participated in the a !1[**\"Go Film!\"**] workshop at the San Joaquin County Office of Education in Stockton, California learning about !1[**camera**], !1[**lighting**], and !1[**sound techniques**]. They worked with other teens to create and release a !1[**short film**], as the !1[**Gaffer**], the !1[**Foley Artist**], and a !1[**Sound Editor**]. In high school, Cass took a class expanding their !1[**film**] knowledge in creating the school's announcements. After graduating, they started a !1[**YouTube channel**] where they posted !1[**short videos**].\n\n\n\nAlong with working in !1[**film**], Cass also participated in !2[**musical theatre**]. They appeared in many productions starting in 2007 with !2[**\"Dear Edwina Jr\"**] at Stockton Civic Theatre as well as many !2[**church musicals**]. In 2009, Cass starred in !2[**\"The Music Man\"**] for a senior project in Manteca, California. Later, they were cast as !2[**Professor Quirrel**] and were set to be the !2[**Musical Director**] for a production of !2[**\"A Very Potter Musical\"**] while directing and developing a !2[**Murder Mystery Dinner**] containing mostly improvised scenes.\n\n\n\nCass has always been interested in !3[**video game development and design**] as they played them often throughout their adolescence. This interest flourished and inspired them to major in !3[**Computer Science**], first at San Joaquin Delta College and then University of the Pacific. They ended up switching their major to Media X to further pursue their passions. During their coursework, they worked with classmates to !3[**develop two games**]: a !3[**combat simulator**] for the tabletop role-playing game Dungeons and Dragons and a !3[**bullet-dodging game**] like the video game Cuphead. Cass intends to continue using !3[**video games**] as an alternate form of storytelling in the future.\n\n\n\n!4[**Music**] has always been part of Cass's life. Inspired by their older sister taking lessons, they decided to learn !4[**piano**] at age 5. They later picked up more instruments such as the !4[**clarinet**], the !4[**cello**], and the !4[**guitar**]. Cass was active in his !4[**high school's band**] program where they took to !4[**arranging**] and !4[**writing**] for their school's !4[**wind ensemble**] and !4[**jazz band**]. They also played !4[**cello**] often in college and assisted East Union High School's !2[**theatre class**] in a production of !2[**\"Next to Normal.\"**] In 2019, Cass sent a video of him playing an !4[**original song**] to the University of the Pacific’s student association. This resulted in Cass playing both !4[**original**] and !4[**cover songs**] at their !4[**Brickyard Series concert**]. Cass now writes !4[**indie pop music**] in their spare time.\n\n\n\nCass hopes to further their storytelling career using their !1[**YouTube channel**] talking about !3[**gaming**], !4[**music**], and languages. Their free time consists of learning German, American Sign Language, Spanish, and Italian. When they're not crafting fantasy novels, Cass plays and runs sessions of various tabletop role-playing games for which they hope to develop a podcast in the future.",
+
+    "blog.title": "Persönlicher Blog",
+    "blog.description": "Zufällige Beiträge, Nachrichten, und andere Gedanken: kommen bald!",
+
+    "nav.previous": "Vor",
+    "nav.portfolio": "Portfolio",
+    "nav.next": "Nächste",
+
+    "nav.page.1": "Seite 1",
+    "nav.page.2": "Seite 2",
+    "nav.page.3": "Seite 3",
+    "nav.page.4": "Seite 4",
+    "nav.page.20": "Seite 20",
+
+    "blog.post.1.title": "",
+    "blog.post.1.author": "",
+    "blog.post.1.date": "",
+    "blog.post.1.text": "",
+
+    "blog.post.2.title": "",
+    "blog.post.2.author": "",
+    "blog.post.2.date": "",
+    "blog.post.2.text": "",
+
+    "blog.post.3.title": "",
+    "blog.post.3.author": "",
+    "blog.post.3.date": "",
+    "blog.post.3.text": "",
+
+    "contact.title": "Kontaktiere mich",
+    "contact.placeholder.name": "Namen *",
+    "contact.placeholder.email": "E-Mail *",
+    "contact.placeholder.message": "Nachrichten *",
+    "contact.submit": "Abschiken"
+}};
