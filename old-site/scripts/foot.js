12a7993682386919935b8aa5506f357d18c430a6
Cass Unterholzner <cityboundforest@gmail.com>
2023-08-19 13:14:07 -0500

    Removing old site code

--- old-site/scripts/foot.js
+++
@@ -1,9 +0,0 @@
-function getFoot() {
-    return `
-        <footer class="site-footer cf">
-            <p class="footer-text">© Copyright Cass Unterholzner 2020-2022. </p>
-        </footer>
-    `;
-}
-
-export default getFoot;
